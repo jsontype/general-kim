ee560114b11fcf6b58e8c84a680a6fa9a7e6444d
jsontype <jsontyper@gmail.com>
2025-01-25 16:56:32 +0900

    i18n

---
+++ src/i18n/locales/en/index.js
@@ -0,0 +1,7 @@
+import movies from "./movies.json"
+
+const en = {
+  movies,
+}
+
+export default en
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 // routerを使うための準備１
 import { BrowserRouter } from "react-router-dom"
+import i18n from "./i18n"
 import "./tailwind.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   // routerを使うための準備２
   <BrowserRouter>
-    <App />
+    <App i18n={i18n} />
   </BrowserRouter>
 )
